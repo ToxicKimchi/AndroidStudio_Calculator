674dbab1303ff6233c5b56376061deaae128e195
lane <lanefuerstenberg@gmail.com>
2019-11-06 10:13:59 -0600

    Fixed typo in operation subclass.

--- app/src/main/java/com/example/calculator/operations/MultiplyDivideOperation.java
+++ app/src/main/java/com/example/calculator/operations/MultiplyDivideOperation.java
@@ -5,14 +5,14 @@ import java.math.BigDecimal;
 public class MultiplyDivideOperation extends Operation {
     @Override
     public String[] setOperators() {
-        return new String[]{"x", "/"};
+        return new String[]{"*", "/"};
     }
 
     @Override
     public BigDecimal handleOperation(String operator, BigDecimal[] variables) {
         BigDecimal result = new BigDecimal(0);
 
-        if (operator.equals("x")) {
+        if (operator.equals("*")) {
             result = variables[0].multiply(variables[1]);
         } else if (operator.equals("/")) {
             result = variables[0].divide(variables[1]);
